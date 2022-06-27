0ccb7ebf9e98799e80ea0882547c4f4c95eabd36
boraxpr <tata-naipawat@hotmail.com>
2022-06-27 11:02:39 +0700

    Update 1:  deprecated code (Legacy jQuery)
    .ready() is deprecated since jQuery 1.8
    .click() is deprecated since jQuery 3.3
    Updated to $(function()) and .on("click", function()) instead.

--- js/app.js
+++ js/app.js
@@ -1,67 +1,66 @@
-$(document).ready(function() {
+$(function () {
     var operandA = 0;
     var operandB = 0;
     var operation = null;
     var temp_entry = 0;
     var history = null;
-    $("button").click(function()
-    {
+    $("button").on("click", function () {
         var input_value = this.value;
 
-        if(input_value==="reset"){
+        if (input_value === "reset") {
             $(".input-screen").val("");
         }
 
-        if($.isNumeric(input_value)){
+        if ($.isNumeric(input_value)) {
 
-            $(".input-screen").val(function(){
-                temp_entry = this.value+input_value;
+            $(".input-screen").val(function () {
+                temp_entry = this.value + input_value;
                 return temp_entry;
             });
         }
 
-        if(input_value==="."){
-            $(".input-screen").val(function(){
+        if (input_value === ".") {
+            $(".input-screen").val(function () {
                 var regExp = /\./i;
-                if(regExp.test(this.value) && input_value==="."){
+                if (regExp.test(this.value) && input_value === ".") {
                     temp_entry = this.value;
                 }
-                else{
-                    temp_entry = this.value+input_value;
+                else {
+                    temp_entry = this.value + input_value;
                 }
                 return temp_entry;
             });
 
         }
 
-        if(input_value==="+" || input_value==="-" || input_value==="x"|| input_value==="/" || input_value==="%"){
-                operandA = temp_entry;
-                operation = this.value;
-                $(".input-screen").val("");
+        if (input_value === "+" || input_value === "-" || input_value === "x" || input_value === "/" || input_value === "%") {
+            operandA = temp_entry;
+            operation = this.value;
+            $(".input-screen").val("");
         }
 
-        if(input_value==="="){
-            if(operandA!=="0" && operation!==null){
+        if (input_value === "=") {
+            if (operandA !== "0" && operation !== null) {
                 operandB = temp_entry;
-                history = operandA+" "+operation+" "+operandB;
+                history = operandA + " " + operation + " " + operandB;
                 $(".history-screen").val(history);
-                $(".input-screen").val(function(){
-                    switch(operation){
+                $(".input-screen").val(function () {
+                    switch (operation) {
                         case "+":
-                            temp_entry = parseFloat(operandA)+parseFloat(operandB);
+                            temp_entry = parseFloat(operandA) + parseFloat(operandB);
                             return temp_entry;
-                            break; 
+                            break;
                         case "-":
-                            temp_entry = parseFloat(operandB)-parseFloat(operandA);
+                            temp_entry = parseFloat(operandB) - parseFloat(operandA);
                             return temp_entry;
                             break;
                         case "x":
-                            temp_entry = parseFloat(operandA)*parseFloat(operandB);
+                            temp_entry = parseFloat(operandA) * parseFloat(operandB);
                             return temp_entry;
                             break;
                         case "/":
-                            var raw = parseFloat(operandA)/parseFloat(operandB);  
-                            raw = raw.toFixed(3);   
+                            var raw = parseFloat(operandA) / parseFloat(operandB);
+                            raw = raw.toFixed(3);
                             temp_entry = raw;
                             return temp_entry;
                             break;
@@ -74,8 +73,8 @@ $(document).ready(function() {
             }
         }
 
-        if(input_value==="+-"){
-            $(".input-screen").val(function(){
+        if (input_value === "+-") {
+            $(".input-screen").val(function () {
                 temp_entry = -temp_entry;
                 return temp_entry;
             });
