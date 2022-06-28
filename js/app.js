ee6353d22cbb0b2c89abaaa909be8ced82dfc2c1
boraxpr <tata-naipawat@hotmail.com>
2022-06-28 22:23:07 +0700

    Bug Fixed 2 : Continuous calculations.
    Previously, It works correctly only when continuously click "=". (Continuously, doing the same operations)
    Now, It's possible to change the operation between the chain.
    TODO: Change value between the chain. (Still bug)

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ $(function () {
     var operation = null;
     var temp_entry = 0;
     var continuous = 0;
+    var cont_value = 0;
     var history = null;
+    $(".input-screen").val("READY");
     $("button").on("click", function () {
         var input_value = this.value;
 
@@ -13,9 +15,9 @@ $(function () {
             operandB = null;
             operation = null;
             temp_entry = 0;
-            cont_entry = 0;
             continuous = 0;
             history = null;
+            cont_value = 0;
             $(".input-screen").val("READY");
             $(".history-screen").val("RESET");
         }
@@ -47,6 +49,7 @@ $(function () {
 
         if (input_value === "+" || input_value === "-" || input_value === "x" || input_value === "/" || input_value === "%") {
             operandA = temp_entry;
+
             operation = this.value;
             $(".input-screen").val("");
         }
@@ -57,7 +60,7 @@ $(function () {
                 continuous = 1;
             }
             else {
-                operandA = temp_entry;
+                operandA = cont_value;
             }
 
             history = operandA + " " + operation + " " + operandB;
@@ -66,24 +69,29 @@ $(function () {
                 switch (operation) {
                     case "+":
                         temp_entry = parseFloat(operandA) + parseFloat(operandB);
+                        cont_value = temp_entry;
                         return temp_entry;
                         break;
                     case "-":
                         temp_entry = parseFloat(operandA) - parseFloat(operandB);
+                        cont_value = temp_entry;
                         return temp_entry;
                         break;
                     case "x":
                         temp_entry = parseFloat(operandA) * parseFloat(operandB);
+                        cont_value = temp_entry;
                         return temp_entry;
                         break;
                     case "/":
                         var raw = parseFloat(operandA) / parseFloat(operandB);
+                        cont_value = raw;
                         raw = raw.toFixed(3);
                         temp_entry = raw;
                         return temp_entry;
                         break;
                     case "%":
                         temp_entry = parseFloat(operandA) % parseFloat(operandB);
+                        cont_value = temp_entry;
                         return temp_entry;
                         break;
                 }
